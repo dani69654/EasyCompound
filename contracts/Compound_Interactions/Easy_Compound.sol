c52947c17a5d37dc5d35f1ca390cd49b182e1f59
dani69654 <60792849+dani69654@users.noreply.github.com>
2020-05-24 12:34:53 +0200

    Add files via upload

--- contracts/Compound_Interactions/Easy_Compound.sol
+++ contracts/Compound_Interactions/Easy_Compound.sol
@@ -40,14 +40,14 @@ contract Easy_Compound is Ownable, Pausable {
 
   using SafeMath for uint256;
 
-  address public daiContractAddress; // Contains Dai SmartContract address || 0x6B175474E89094C44Da98b954EedeAC495271d0F ||
-  address public cDaiContractAddress; // Contains cDai SmartContract address || 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643 ||
-  address public cEthContractAddress; // Contains eEth SmartContract address || 0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5 ||
+  address private daiContractAddress; // Contains Dai SmartContract address || 0x6B175474E89094C44Da98b954EedeAC495271d0F ||
+  address private cDaiContractAddress; // Contains cDai SmartContract address || 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643 ||
+  address private cEthContractAddress; // Contains eEth SmartContract address || 0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5 ||
 
-  uint256 public contractEtherBalance; // Total amount of Ether in Escrow
-  uint256 public contractDaiBalance; // Total amount of Dai in Escrow
-  uint256 public contractcEtherBalance; // Total amount of cEther in Escrow
-  uint256 public contractcDaiBalance; // Total amount of cDai in Escrow
+  uint256 private contractEtherBalance; // Total amount of Ether in Escrow
+  uint256 private contractDaiBalance; // Total amount of Dai in Escrow
+  uint256 private contractCethBalance; // Total amount of cEther in Escrow
+  uint256 private contractCdaiBalance; // Total amount of cDai in Escrow
 
 
   event daiChanged (address _addr, uint256 _amount); // Emit address[0] and _amount increased/decreased
@@ -59,24 +59,23 @@ contract Easy_Compound is Ownable, Pausable {
     cEthContractAddress = _cEthContractAddress;
   }
 
-  mapping (address => uint256) private EtherAddrBalance; // Total amount of Ether owner by address in Escrow
-  mapping (address => uint256) private DaiAddrBalance; // Total amount of Dai owned by address in Escrow
-  mapping (address => uint256) private cEtherAddrBalance; // Total amount of cEther owner by address in Escrow
-  mapping (address => uint256) private cDaiAddrBalance;// Total amount of cDai owner by address in Escrow
+  mapping (address => uint256) private etherAddrBalance; // Total amount of Ether owner by address in contract
+  mapping (address => uint256) private daiAddrBalance; // Total amount of Dai owned by address in contract
+  mapping (address => uint256) private cetherAddrBalance; // Total amount of cEther owner by address in contract
+  mapping (address => uint256) private cdaiAddrBalance;// Total amount of cDai owner by address in contract
 
 
-  function depositEther () public payable returns (uint256) {
+  function depositEther () public payable returns (bool){
     // Deposit Ether in contract
-    // Returns balance of msg.sender
 
-    EtherAddrBalance[msg.sender] = EtherAddrBalance[msg.sender].add(msg.value);
+    etherAddrBalance[msg.sender] = etherAddrBalance[msg.sender].add(msg.value);
     contractEtherBalance = contractEtherBalance.add(msg.value);
     emit ethChanged (msg.sender, msg.value);
-    return EtherAddrBalance[msg.sender];
+    return true;
   }
 
 
-  function depositDaiTokens (uint256 _amount) public returns (uint256) {
+  function depositDaiTokens (uint256 _amount) public returns (bool) {
     /*
     @Dev --> You must approve this contract to use the function 'transferFrom'
          --> Uso cDaiContractAddress.approve(thisContractAddr, amount)
@@ -86,10 +85,10 @@ contract Easy_Compound is Ownable, Pausable {
     */
     bool check = Erc20(daiContractAddress).transferFrom(msg.sender,address(this), _amount);
     require (check == true, "Dai transfer failed");
-    DaiAddrBalance[msg.sender] = DaiAddrBalance[msg.sender].add(_amount);
+    daiAddrBalance[msg.sender] = daiAddrBalance[msg.sender].add(_amount);
     contractDaiBalance = contractDaiBalance.add(_amount);
     emit daiChanged (msg.sender, _amount);
-    return DaiAddrBalance[msg.sender];
+    return true;
   }
 
 
@@ -103,18 +102,23 @@ contract Easy_Compound is Ownable, Pausable {
   */
 
 
-  function supplyEthToCompound (uint256 _amount) public payable whenNotPaused returns (uint256) {
-    require (EtherAddrBalance[msg.sender] >= _amount, "Not enough balance");
+  function supplyEthToCompound (uint256 _amount) public payable whenNotPaused returns (bool){
+
+    //Check requirement and mint
+    require (etherAddrBalance[msg.sender] >= _amount, "Not enough balance");
     CEth(cEthContractAddress).mint.value(_amount).gas(20000000000)(); // No return, reverts on error
-    EtherAddrBalance[msg.sender] = EtherAddrBalance[msg.sender].sub(_amount);
 
-    uint256 cEthExchRate = CEth(cEthContractAddress).exchangeRateCurrent();
-    cEtherAddrBalance[msg.sender] = cEtherAddrBalance[msg.sender].add(cEthExchRate.mul(_amount));
-
+    //Subtract Ether _amount from contract and user balances
+    etherAddrBalance[msg.sender] = etherAddrBalance[msg.sender].sub(_amount);
     contractEtherBalance = contractEtherBalance.sub(_amount);
-    contractcEtherBalance = contractcEtherBalance.add(cEthExchRate.mul(_amount));
 
-    return cEtherAddrBalance[msg.sender];
+    //Increase Ether _amount in contract and user balances
+
+    uint256 cEthExchRate = getCethExchangeRate();
+    cetherAddrBalance[msg.sender] = cetherAddrBalance[msg.sender].add((_amount.mul(1000000000000000000)).div(cEthExchRate));
+    contractCethBalance = contractCethBalance.add(cetherAddrBalance[msg.sender]);
+
+    return true;
   }
 
 
@@ -126,24 +130,30 @@ contract Easy_Compound is Ownable, Pausable {
   Returns the amount of cDai owned by msg.sender
   */
 
-  function supplyErc20ToCompound (uint256 _numTokensToSupply) public whenNotPaused returns (uint256) {
+  function supplyErc20ToCompound (uint256 _amount) public whenNotPaused returns (bool) {
 
-    require (DaiAddrBalance [msg.sender] >= _numTokensToSupply, "Not enough balance");
+    require (daiAddrBalance [msg.sender] >= _amount, "Not enough balance");
 
     Erc20 underlying = Erc20(daiContractAddress);
     CErc20 cToken = CErc20(cDaiContractAddress);
 
-    underlying.approve(cDaiContractAddress, _numTokensToSupply);
-    // Mint cTokens
-    uint256 check = cToken.mint(_numTokensToSupply);
+    underlying.approve(cDaiContractAddress, _amount);
+
+    uint256 check = cToken.mint(_amount);
     require (check == 0, "Mint failed");
-    DaiAddrBalance[msg.sender] = DaiAddrBalance[msg.sender].sub(_numTokensToSupply);
 
+    //Decreasing contract and user Dai balances -= _amount
+    daiAddrBalance[msg.sender] = daiAddrBalance[msg.sender].sub(_amount);
+    contractDaiBalance = contractDaiBalance.sub(_amount);
+
+    //Increasing contract and user cDai balances += _amount
     uint256 cDaiExchRate = CErc20(cDaiContractAddress).exchangeRateCurrent();
-    cDaiAddrBalance[msg.sender] = cDaiAddrBalance[msg.sender].add(cDaiExchRate.mul(_numTokensToSupply));
+    cdaiAddrBalance[msg.sender] = cdaiAddrBalance[msg.sender].add((_amount.mul(1000000000000000000)).div(cDaiExchRate));
 
-    contractDaiBalance = contractDaiBalance.sub(_numTokensToSupply);
-    contractcDaiBalance = contractcDaiBalance.add(cDaiExchRate.mul(_numTokensToSupply));
+
+    contractCdaiBalance = contractCdaiBalance.add((_amount.mul(1000000000000000000)).div(cDaiExchRate));
+
+    return true;
   }
 
 
@@ -159,23 +169,27 @@ contract Easy_Compound is Ownable, Pausable {
     require(check == 0, "Redeem failed");
 
     if(_cTokenAddress == cDaiContractAddress){
-      cDaiAddrBalance[msg.sender] = cDaiAddrBalance[msg.sender].sub(_amount);
-      contractcDaiBalance = contractcDaiBalance.sub(_amount);
+      cdaiAddrBalance[msg.sender] = cdaiAddrBalance[msg.sender].sub(_amount);
+      contractCdaiBalance = contractCdaiBalance.sub(_amount);
+
       uint cDaiExchRate = CErc20(cDaiContractAddress).exchangeRateCurrent();
-      contractDaiBalance = contractDaiBalance.add(_amount.div(cDaiExchRate));
-      cDaiAddrBalance[msg.sender] = cDaiAddrBalance[msg.sender].add(_amount.div(cDaiExchRate));
+
+      contractDaiBalance = contractDaiBalance.add((_amount.mul(cDaiExchRate)).div(1000000000000000000));
+      daiAddrBalance[msg.sender] = daiAddrBalance[msg.sender].add((_amount.mul(cDaiExchRate)).div(1000000000000000000));
       return true;
     }
 
     if(_cTokenAddress == cEthContractAddress){
-      cEtherAddrBalance[msg.sender] = cEtherAddrBalance[msg.sender].sub(_amount);
-      contractcEtherBalance = contractcEtherBalance.sub(_amount);
+      cetherAddrBalance[msg.sender] = cetherAddrBalance[msg.sender].sub(_amount);
+      contractCethBalance = contractCethBalance.sub(_amount);
+
       uint256 cEthExchRate = CEth(cEthContractAddress).exchangeRateCurrent();
-      contractcEtherBalance = contractcEtherBalance.add(_amount.div(cEthExchRate));
-      cEtherAddrBalance[msg.sender] = cEtherAddrBalance[msg.sender].add(_amount.div(cEthExchRate));
+
+      contractEtherBalance = contractEtherBalance.add((_amount.mul(cEthExchRate)).div(1000000000000000000));
+      etherAddrBalance[msg.sender] = etherAddrBalance[msg.sender].add((_amount.mul(cEthExchRate)).div(1000000000000000000));
       return true;
     }
-    revert(); //This should never happen
+    revert('fatal'); //This should never happen ---> This could trigger a lockdown? Fatal error
   }
 
 
@@ -185,9 +199,9 @@ contract Easy_Compound is Ownable, Pausable {
     //Emit the user address and amount withdrawn
 
     require (_amount > 0.01 ether, "Min 0.01 ether");
-    require (EtherAddrBalance[msg.sender] >=  _amount , "Not enough funds");
+    require (etherAddrBalance[msg.sender] >=  _amount , "Not enough funds");
 
-    EtherAddrBalance[msg.sender] = EtherAddrBalance[msg.sender].sub(_amount);
+    etherAddrBalance[msg.sender] = etherAddrBalance[msg.sender].sub(_amount);
 
     contractEtherBalance = contractEtherBalance.sub(_amount);
 
@@ -204,9 +218,9 @@ contract Easy_Compound is Ownable, Pausable {
     //Emit the user address and amount withdrawn
 
 
-    require (DaiAddrBalance[msg.sender] >= _amount, "Not enough funds");
+    require (daiAddrBalance[msg.sender] >= _amount, "Not enough funds");
 
-    DaiAddrBalance[msg.sender] = DaiAddrBalance[msg.sender].sub(_amount);
+    daiAddrBalance[msg.sender] = daiAddrBalance[msg.sender].sub(_amount);
 
     contractDaiBalance = contractDaiBalance.sub(_amount);
 
@@ -224,11 +238,38 @@ contract Easy_Compound is Ownable, Pausable {
   //Getter and setter functions
 
   /*
-    Return the balances of Easy_Compound:
-    > cDai balance
+  Returns: the balances of Easy_Compound:
+    > Ether contract balance
     > Dai balance
+    > cDai balance
     > CEth balance
   */
+
+  function getContractEtherBalance () public view returns (uint256){
+
+    //Returns variable 'contractEtherBalance';
+    return contractEtherBalance;
+  }
+
+  function getContractDaiBalance () public view returns (uint256){
+
+    //Returns variable 'contractDaiBalance';
+    return contractDaiBalance;
+  }
+
+  function getContractCethBalance () public view returns (uint256){
+
+    //Returns variable 'contractCethBalance';
+    return contractCethBalance;
+  }
+
+  function getContractCdaiBalance () public view returns (uint256){
+
+    ////Returns variable 'contractCdaiBalance';
+    return contractCdaiBalance;
+  }
+
+
 
   function getCErc20Balance () public returns (uint256){
     return CErc20(cDaiContractAddress).balanceOfUnderlying(address(this));
@@ -244,16 +285,16 @@ contract Easy_Compound is Ownable, Pausable {
 
   /* Return C Tokens exchange rates */
 
-  function getcDaiExchangeRate () public returns (uint256){
+  function getCdaiExchangeRate () public returns (uint256){
     return CErc20(cDaiContractAddress).exchangeRateCurrent();
   }
 
-  function getcEthExchangeRate () public returns (uint256){
+  function getCethExchangeRate () public returns (uint256){
     return CEth(cEthContractAddress).exchangeRateCurrent();
   }
 
 
-  /* Return User balances in escrow and compound
+  /* Return User balances in escrow
      > Get user Ether balance in escrow
      > Get user Dai balance in escrow
      > Get user cEther balance in escrow
@@ -261,27 +302,28 @@ contract Easy_Compound is Ownable, Pausable {
   */
 
   function getUserEthBalance (address _user) public view returns (uint256){
-    return EtherAddrBalance[_user];
+    return etherAddrBalance[_user];
   }
 
   function getUserDaiBalance (address _user) public view returns (uint256){
-    return DaiAddrBalance[_user];
+    return daiAddrBalance[_user];
   }
 
-  function getUsercEthBalance (address _user) public view returns (uint256){
-    return cEtherAddrBalance[_user];
+  function getUserCethBalance (address _user) public view returns (uint256){
+    return cetherAddrBalance[_user];
   }
 
-  function getUsercDaiBalance (address _user) public view returns (uint256){
-    return cDaiAddrBalance[_user];
+  function getUserCdaiBalance (address _user) public view returns (uint256){
+    return cdaiAddrBalance[_user];
   }
 
 
 
-  /* Setters for main contracts
-      > Dai address
-      > cDai address
-      > cEth address
+  /* Setters for compound and erc20 contracts
+  Requirements: Contract must be paused
+    > Dai address
+    > cDai address
+    > cEth address
   */
 
 
---
+++ test/0_Test_Ez_Compound.js
@@ -0,0 +1,182 @@
+const Easy_Compound = artifacts.require('Easy_Compound');
+const truffleAssert = require ("truffle-assertions");
+const Web3 = require('web3');
+
+//Importing contracts ABIs
+const abi_Easy_Compound = require('../test/abis/abi_Easy_Compound.js');
+const abi_Dai_Contract = require('../test/abis/abi_Dai_Contract.js');
+const abi_cDai_Contract = require('../test/abis/abi_cDai_Contract.js');
+const abi_cEth_Contract = require('../test/abis/abi_cEth_Contract.js');
+
+
+
+//Declaring contract addresses
+
+const cEthContractAddress = '0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5';
+const daiContractAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const cDaiContractAddress = "0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643";
+
+
+//Instancing web3 interfaces
+const compoundCEthContract = new web3.eth.Contract(abi_cEth_Contract, cEthContractAddress);
+const compoundDaiContract = new web3.eth.Contract(abi_Dai_Contract, daiContractAddress);
+const compoundcDaiContract = new web3.eth.Contract(abi_cDai_Contract, cDaiContractAddress);
+
+
+
+
+contract("Easy_Compound", async function(accounts){
+
+
+let instance;
+let contractInstance;
+const oneToken = Web3.utils.toWei('1', 'ether');
+const almostOneToken = Web3.utils.toWei('0.999', 'ether');
+
+
+const web3 = new Web3('http://127.0.0.1:8545');
+
+
+  before (async function(){
+    instance = await Easy_Compound.new(daiContractAddress,cDaiContractAddress,cEthContractAddress);
+    contractInstance = new web3.eth.Contract(abi_Easy_Compound, instance.address, {from: accounts[0]});
+    console.log('Setup Done');
+  })
+
+
+
+
+
+  it("Should deposit Ether.", async function (){
+
+    //Depositing Ether
+    await contractInstance.methods.depositEther().send({from: accounts[0],value:oneToken});
+
+    //Checking Ether balance
+    let result = await contractInstance.methods.getUserEthBalance(accounts[0]).call();
+
+    //Asserts user Ether balance == 1 Ether
+    assert (result == oneToken);
+
+  })
+
+
+  it("Should move dai from user to Dai escrow.", async function(){
+
+    //Granting allowance to smart contract to grab money from accounts[0];
+    await compoundDaiContract.methods.approve(instance.address, oneToken).send({from:accounts[0]});
+
+    //Moving the authorized Dai amount from user to escrow
+    await contractInstance.methods.depositDaiTokens(oneToken).send({from:accounts[0]});
+
+    //Returns dai balance in escrow. Since that was the only Dai deposit, must be oneToken
+    let result = await contractInstance.methods.getDaiBalance().call();
+    assert (result == oneToken);
+
+  })
+
+
+  it("Should supply Ether to Compound.", async function(){
+
+    //Depositing Ether in exchange of cEth
+    await contractInstance.methods.supplyEthToCompound(oneToken).send({from: accounts[0],
+    gasLimit: web3.utils.toHex(1500000),
+    gasPrice: web3.utils.toHex(200000000000)});
+
+
+    //Checking cEth accounts[0] balance
+    let result = await contractInstance.methods.getUsercEthBalance(accounts[0]).call();
+    console.log("User has: " + result.toString() + " cEth");
+
+    //Checking cEth in contract by using the cToken abstaction
+    let result1 = await contractInstance.methods.getCEthBalance().call();
+    console.log("Contract has: " +result.toString() + " cEth");
+
+    //Checking cEth in contract by using the contract variable
+    let result2 = await contractInstance.methods.getcontractcEtherBalance().call();
+    console.log("Contract has: " +result.toString() + " cEth");
+
+    //The 3 results obtained should be the same since all the balances were 0
+    assert(result == result1 &&  result1 == result2);
+
+    //User should have 0 Ether because they were just swapped into cEth
+    result = await contractInstance.methods.getUserEthBalance(accounts[0]).call();
+    assert(result == 0);
+
+  })
+
+
+  it("Should redeem cEth.", async function(){
+
+    //Redeeming all the cEth tokens we deposited before
+    let userToken = await contractInstance.methods.getCEthBalance().call();
+    let check = await contractInstance.methods.redeemCtokens(userToken,cEthContractAddress).send({from: accounts[0],
+    gasLimit: web3.utils.toHex(1500000),
+    gasPrice: web3.utils.toHex(200000000000)});
+
+    //Asserts accounts[0] has now 0 cEth
+    let result = await contractInstance.methods.getUsercEthBalance(accounts[0]).call();
+    assert(result == 0);
+
+    //Asserts  contract has now 0 cEth
+    result = await contractInstance.methods.getcontractcEtherBalance().call();
+    assert(result == 0);
+
+    //Assertes accounts[0] has ≃ 1 Ether, will not be one because of fees
+    result = await contractInstance.methods.getUserEthBalance(accounts[0]).call();
+    assert(result > almostOneToken);
+  })
+
+
+  it("Should supply Dai to Compound.", async function(){
+
+
+    //Depositing all user Dai in exchange of cDAI and swapping them in cDai
+    await contractInstance.methods.getUserDaiBalance(accounts[0]).call().then(async function(res){
+      await contractInstance.methods.supplyErc20ToCompound(res).send({
+        from: accounts[0],
+        gasLimit: web3.utils.toHex(1500000),
+        gasPrice: web3.utils.toHex(200000000000)
+      })
+    })
+
+    //Asserts contract's Dai balance == 0
+    let result = await contractInstance.methods.getDaiBalance().call();
+    assert(result == 0);
+    result = await contractInstance.methods.getContractDaiBalance().call();
+    assert(result == 0);
+
+    //Asserts user Dai balance == 0
+    result = await contractInstance.methods.getUserDaiBalance(accounts[0]).call();
+    assert(result == 0);
+
+    //Asserting user and contract got credited cDAI
+    let userCdaiBalance = await contractInstance.methods.getUserCdaiBalance(accounts[0]).call();
+    let contractCdaiBalance = await contractInstance.methods.getUserCdaiBalance(accounts[0]).call();
+
+  })
+
+
+  it("should redeem cDai to Dai: ", async function(){
+
+    //Asserting user has no Dai
+    let result = await contractInstance.methods.getUserDaiBalance(accounts[0]).call();
+    assert (result == 0);
+
+    //Redeeming cDai for Dai
+    let userToken = await contractInstance.methods.getUserCdaiBalance(accounts[0]).call();
+    let check = await contractInstance.methods.redeemCtokens(userToken,cDaiContractAddress).send({from: accounts[0],
+    gasLimit: web3.utils.toHex(1500000),
+    gasPrice: web3.utils.toHex(200000000000)});
+
+    //Asserting user got Dai back
+    let userDaibalance = await contractInstance.methods.getUserDaiBalance(accounts[0]).call();
+    assert (userDaibalance >= oneToken); //Will be a bit more than one Dai thanks to fees
+
+    //Asserting user and contract balances are the same
+    let contractDaibalance = await contractInstance.methods.getDaiBalance().call();
+    assert (userDaibalance == contractDaibalance);
+
+  })
+
+})
